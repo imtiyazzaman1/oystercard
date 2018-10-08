a84648a833a33b866ba1605ef274fe57c94d7eb7
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-08 14:46:03 +0100

    Step 3: added Oystercard class with initial balance = 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,9 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  describe "#balance" do
+    it "should report intial balance as 0" do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
