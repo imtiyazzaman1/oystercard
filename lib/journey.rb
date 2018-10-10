c1255558ed5fede8077232af390de5067ae9280a
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-10 20:51:22 +0100

    removed intialise arugments and made a start class

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,8 +2,12 @@ class Journey
 
   attr_reader :entry_station, :exit_station
 
-  def initialize(entry_station = "")
-    @entry_station = entry_station
+  def initialize
+    @entry_station
+  end
+
+  def start(station = "")
+    @entry_station = station
   end
 
   def end(station = "")
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,40 +1,37 @@
 require 'journey'
+require 'oystercard'
 require 'pry'
 describe Journey do
   let(:station) { double :station }
   let(:station_2) { double :station }
 
   it "has an entry station" do
-    station = Station.new("Aldgate East", 1)
-    subject = Journey.new(station)
+    subject.start(station)
     expect(subject.entry_station).to eq station
   end
 
   it "should have an exit station" do
-    station = Station.new("Aldgate East", 1)
-    subject = Journey.new(station)
-    station2 = Station.new("Richmond", 4)
-    subject.end(station2)
-    expect(subject.exit_station).to eq station2
+    subject.end(station_2)
+    expect(subject.exit_station).to eq station_2
   end
 
   describe "#fare" do
     it "should return the minimum fare" do
-      subject = Journey.new(station)
-      subject.end(station_2)
       allow(station).to receive(:empty?).and_return false
       allow(station_2).to receive(:empty?).and_return false
+      subject.start(station)
+      subject.end(station_2)
       expect(subject.fare).to eq Oystercard::MIN_FARE
     end
 
     it "should return the penalty fare for a journey without an entry station" do
-      subject = Journey.new()
+      subject.start
       expect(subject.fare).to eq Oystercard::PENALTY_FARE
     end
 
     it "should return the penalty fare for a journey without an exit station" do
       allow(station).to receive(:empty?).and_return false
-      subject = Journey.new(station)
+      subject.start(station)
       subject.end
       expect(subject.fare).to eq Oystercard::PENALTY_FARE
     end
